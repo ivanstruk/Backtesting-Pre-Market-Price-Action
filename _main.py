86941a2ba445429a8035e5a86b431bc986338d42
ivanstruk <39165741+ivanstruk@users.noreply.github.com>
2019-11-19 15:48:51 +0100

    More refactoring.
    The code is now more respondent to changes, and works properly. Issues remain in data consolidation and presentation.

---
+++ _main.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 
+
+@author: Ivan Struk
+
+"""
+
+from definition import get_er_dates
+from master import spy
+from master import qqq
+from master import hunt_hist
+from master import get_hist
+from master import get_hist2
+import pandas as pd
+
+
+stocks = spy() + qqq()
+stocks = ["TSLA", "MSFT", "AAPL"]
+
+
+def execute(symbol):
+    stocks = symbol
+    stockz = []
+    re1 = []
+    re2 = []
+    re3 = []
+    
+    for i in stocks:
+        print("Getting historical results for {}".format(i))
+        dfw = pd.DataFrame(get_er_dates(i))
+        name = i
+        length_ = sum(dfw["Binary_E"])
+        if length_ == 0:
+            print("Skipping - No Data")
+            pass
+        else:
+            result1 = round(((sum(dfw["PreM and Intra +"])/length_)*1),4)
+            zero_denom_test = sum(dfw["Pre-Market D+"])
+            if zero_denom_test == 0:
+                result2 = 0
+                continue
+            else:
+                result2 = round(((sum(dfw["PreM and Intra +"])/sum(dfw["Pre-Market D+"]))*1),4)
+                result3 = round(((sum(dfw["PreM+ and Intra -"])/sum(dfw["Pre-Market D+"]))*1),4)
+            stockz.append(name)
+            re1.append(result1)
+            re2.append(result2)
+            re3.append(result3)
+    
+    stock_parser = {
+        "Symbol" : []    
+    }
+    
+    
+    print("Parsing results dataframe...")
+    results_df = pd.DataFrame(stock_parser)
+    results_df["Symbol"] = stockz
+    results_df["- increases in both premarket and intraday x% of the time."] = re1
+    results_df["- if increasing in premarket has a x% chance of increasing in intraday."] = re2
+    results_df["- if increasing in premarket has a x% change of falling in intraday."] = re3
+    print("Saving...")
+    results_df.to_csv("Main Output.csv")
+    return print("Done")
+
+
+def test_sample_contribution(symbol):
+    stocks = symbol
+    stock_names = []
+    premarket_att = []
+    
+    print("Crunching numbers ... (this may take a while)")
+    for i in stocks:
+        df_sub = pd.DataFrame(get_hist(i))
+        df = df_sub.fillna(value=0)
+        result = sum(df["ABS PM CHN / ABS ID + PM"])/len(df["ABS PM CHN / ABS ID + PM"])
+        name = i
+        print(name, result)
+        stock_names.append(name)
+        premarket_att.append(result)
+        
+    stock_parser = {
+        "Symbol" : [],
+        "Result" : []
+    }
+    results_df = pd.DataFrame(stock_parser)
+    results_df["Symbol"] = stock_names
+    results_df["Result"] = premarket_att
+    label = "test_sample_contribution"
+    print("Saving to csv...")
+    results_df.to_csv("{}.csv".format(label))
+    print("Result:")
+    
+    average_premarket_weight = sum(results_df["Result"])/len(results_df["Result"])
+    print("On average premarket trading contributes to ",
+          (round(average_premarket_weight,4)*100), 
+          "% of daily price action within the selected sample.")
+    
+
+def test_sample_indication(symbol):
+    stock_names = []
+    results = []
+    stocks = symbol
+    print("This may take a while if you have defined a large number of stocks")
+    
+    for i in stocks:
+        df = pd.DataFrame(get_hist2(i))
+        #df = df_sub.fillna(value=0)
+        result = round((sum(df["H: Both True"]))/(len(list(df["H: Both True"]))),4)
+        name = i
+        print(name, result)
+        stock_names.append(name)
+        results.append(result)
+        
+    stock_parser = {
+        "Symbol" : [],
+        "Result" : []
+    }
+    results_df = pd.DataFrame(stock_parser)
+    results_df["Symbol"] = stock_names
+    results_df["Result"] = results
+    label = "test_sample_indication"
+    print("Saving to csv...")
+    results_df.to_csv("{}.csv".format(label))
+    print("Result:")
+    
+    final_result = sum(results_df["Result"])/len(results_df["Result"])
+    print("A stock increasing or decreasing in value during premarket has a ",
+          (round(final_result,4)*100), 
+          "% probability of maintaining price action in the same direction.")
+    
+def test_sample_long(symbol):
+    stocks = symbol
+    stock_names = []
+    results = []
+    
+    print("This may take a while if you have defined a large number of stocks.")
+    
+    for i in stocks:
+        df = pd.DataFrame(hunt_hist(i))
+        #df = df_sub.fillna(value=0)
+        result = round((sum(df["Temp Calc1"]))/(sum(df["Pre-Market D+"])),4)
+        name = i
+        print(name, result)
+        stock_names.append(name)
+        results.append(result)
+        
+    stock_parser = {
+        "Symbol" : [],
+        "Result" : []
+    }
+    results_df = pd.DataFrame(stock_parser)
+    results_df["Symbol"] = stock_names
+    results_df["Result"] = results
+    label = "test_sample_long"
+    print("Saving to csv...")
+    results_df.to_csv("{}.csv".format(label))
+    print("Result:")
+    
+    final_result = sum(results_df["Result"])/len(results_df["Result"])
+    print("On average for the provided sample, "
+          "a stock increasing in value during premarket has a ",
+          (round(final_result,4)*100), 
+          "% probability of further increasing in intraday.")
+
+   
+test_sample_contribution(stocks)
+test_sample_indication(stocks)
+test_sample_long(stocks)
+execute(stocks)
--- definition.py
+++ definition.py
@@ -10,38 +10,15 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import requests
-
 import datetime
-
 from pandas.plotting import register_matplotlib_converters
-register_matplotlib_converters()
 import bs4
 import re
 
-
-"""  Markets
-"""
-
-def spy():
-    spy_1 = pd.read_csv("d_SP500.csv", encoding = "utf-8", usecols= ["Symbol"])
-    spy_list = spy_1["Symbol"]
-    real_spy_list = list(spy_list)
-    spy = real_spy_list
-    return spy
-
-def qqq():
-	qqq_1 = pd.read_csv("d_Nasdaq100.csv")
-	qqq_list = qqq_1["Ticker"]
-	real_qqq_list = list(qqq_list)
-	qqq = real_qqq_list
-	return qqq
+register_matplotlib_converters()
 
 
-"""  These are the history modules. They pull data from Yahoo Finance
-     and also handle most of the calculations.
-"""
-
-def get_hist(symbol):
+def get_history(symbol):
     target_tickers = []
     target_tickers.append(symbol)
     fetch = yf.download(tickers = target_tickers,
@@ -52,6 +29,7 @@ def get_hist(symbol):
                 prepost = True,
                 treads = True,
                 proxy = None)
+
     df = pd.DataFrame(data=fetch, index=None).reset_index(drop=False)
     df["Pre-Market Change"] = df["Open"] - df["Close"].shift(+1)
     df["Intraday Change"] = df["Close"] - df["Open"]
@@ -64,12 +42,14 @@ def get_hist(symbol):
     df["ABS PM Chn"] = df["Pre-Market Change"].abs()
     df["ABS ID Chn"] = df["Intraday Change"].abs()
     df["ABS ID + PM"] = df["ABS ID Chn"] + df["ABS PM Chn"]
-    df["ABS PM CHN / ABS ID + PM"] = round((df["ABS PM Chn"] / df["ABS ID + PM"]),2)
+    df["ABS PM CHN / ABS ID + PM"] = round((df["ABS PM Chn"] / df["ABS ID + PM"]),2) # this is a good one
     df["Intraday HL Range"] = df["High"] - df["Low"]
     df1 = df.iloc[1:]
     return df1
+    
 
-def get_hist2(symbol):
+
+def get_hist(symbol):
     target_tickers = []
     target_tickers.append(symbol)
     fetch = yf.download(tickers = target_tickers,
@@ -80,6 +60,7 @@ def get_hist2(symbol):
                 prepost = True,
                 treads = True,
                 proxy = None)
+
     df = pd.DataFrame(data=fetch, index=None).reset_index(drop=False)
     df["Pre-Market Change"] = df["Open"] - df["Close"].shift(+1)
     df["Intraday Change"] = df["Close"] - df["Open"]
@@ -89,8 +70,8 @@ def get_hist2(symbol):
     df2 = df.iloc[1:]
     return df2
 
-
 def hunt_hist(symbol):
+    #symbol = "TSLA"
     target_tickers = []
     target_tickers.append(symbol)
     fetch = yf.download(tickers = target_tickers,
@@ -118,18 +99,13 @@ def hunt_hist(symbol):
     df["Intraday D-"] = np.where(df['Intraday Change']<0, 1, 0)
     df["Temp Calc"] = df["Pre-Market D+"]+df["Intraday D+"]
     df["Temp Calc1"] = np.where(df['Temp Calc']>1, 1, 0)
-   
+    df = df.drop(axis=1, labels = ["Temp Calc", "H: Both True", "H1: Increasing", "H2: Decreasing", "Premarket D", "Intraday D"])
     df3 = df.iloc[1:]
     return df3
 
-
 def getDateList(ticker):
-    print('Searching for earnings report dates for '+ticker)
-    url_p_1 = "https://www.sec.gov/cgi-bin/"
-    url_p_2 = "browse-edgar?type=10-&dateb=&owner=include&count=100&"
-    url_p_3 = 'action=getcompany&CIK=%s'
-    url_ag = url_p_1 + url_p_2 + url_p_3
-    url =  url_ag % ticker
+    #print('Searching for earnings report dates for '+ticker)
+    url = 'https://www.sec.gov/cgi-bin/browse-edgar?type=10-&dateb=&owner=include&count=100&action=getcompany&CIK=%s' % ticker
     headerInfo={'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url,headers=headerInfo)
     response.raise_for_status()
@@ -147,8 +123,8 @@ def getDateList(ticker):
         tdElems = tr.select('td')
         if len(tdElems) == 5 and dateFind.search(tdElems[3].getText()) != None:
             date = tdElems[3].getText()
-            converted1 = datetime.datetime.strptime(date,'%Y-%m-%d')
-            converted = converted1.strftime('%m/%d/%Y')
+            #converted = datetime.datetime.strptime(date,'%Y-%m-%d').strftime('%m/%d/%Y')
+            converted = datetime.datetime.strptime(date,"%Y-%m-%d").strftime("%Y-%m-%d")
             dateList.append([converted])
     return dateList
 
@@ -158,8 +134,6 @@ def getEarnings(ticker, header=True):
     df = pd.DataFrame(dateList)
     df = df.iloc[1:]
     return df
-
-
 
 def give_df(symbol):
     x = symbol
@@ -193,9 +167,24 @@ def give_df(symbol):
     df_hist = df1
     df_er = get_er(x)
     df_merged = df_hist.merge(df_er, on = "Date", how = "left")
-    df_merged.to_csv("Merged Test.csv")
+    #df_merged.to_csv("Merged Test.csv")
     return df_merged
 
-
-
-
+def get_er_dates(symbol):
+        dfq = give_df(symbol)
+        dfq["Binary_E"] = np.where(dfq['Event'] == "Earnings", 1, 0)
+        #dfq["Binary_-1"] = np.where(dfq["Binary_E"].shift(-1) == 1, 1, 0)
+        dfq["Pre-Market Change"] = dfq["Open"] - dfq["Close"].shift(+1)
+        dfq["Intraday Change"] = dfq["Close"] - dfq["Open"]
+        dfq["Pre-Market D+"] = np.where(dfq['Pre-Market Change']>0, 1, 0)
+        dfq["Intraday D+"] = np.where(dfq['Intraday Change']>0, 1, 0)
+        dfq["Pre-Market D-"] = np.where(dfq['Pre-Market Change']<0, 1, 0)
+        dfq["Intraday D-"] = np.where(dfq['Intraday Change']<0, 1, 0)
+        dfq["PreM and Intra +"] = np.where(dfq['Pre-Market D+'] +
+                                           dfq["Intraday D+"] > 1, 1, 0)
+        dfq["PreM+ and Intra -"] = np.where(dfq['Pre-Market D+'] +
+                                           dfq["Intraday D-"] > 1, 1, 0)
+        dfw = dfq.dropna()
+        return dfw
+    
+    
